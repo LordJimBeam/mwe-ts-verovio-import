ed559813c0d76d4b8e7b18a06e635de1597e91be
Christian Stroehmeier <stroemi@uni-paderborn.de>
2018-11-23 16:16:04 +0100

    Actually defined some types as per documentation

--- vendor/verovio.d.ts
+++ vendor/verovio.d.ts
@@ -1,1 +1,28 @@
-declare module 'verovio'
+declare module 'verovio' {
+  interface VerovioOptions {
+    pageHeight?: number;
+    pageWidth?: number;
+    ignoreLayout?: number;
+    border?: number;
+    scale?: number;
+  }
+
+  interface VerovioToolkit {
+    edit: (editorAction: string) => boolean;
+    getElementsAtTime: (millisec: number) => object;
+    getElementAttr: (xmlId: string) => object;
+    getLog: () => string;
+    getMEI: (page?: number, scoreBased?: number) => string;
+    getPageCount: () => number;
+    getPageWithElement: (xmlId: string) => number;
+    getTimeForElement: (xmlId: string) => number;
+    getVersion: () => string;
+    loadData: (data: string) => void;
+    redoLayout: () => void;
+    renderData: (data: string, options?: VerovioOptions) => string;
+    renderPage: (pageNumber: number, options?: VerovioOptions) => string;
+    renderToMidi: (options?: VerovioOptions) => string;
+    setOptions: (options: VerovioOptions) => void;
+  }
+  const verovio: { toolkit: new () => any };
+}
