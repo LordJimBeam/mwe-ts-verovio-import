d824f2c975a981206575c1c8b5b9f7120246b437
Simon Waloschek <simon.waloschek@posteo.de>
2018-11-23 10:00:49 +0100

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Minimal Working Example
---
+++ src/mwe.ts
@@ -0,0 +1,9 @@
+
+
+import * as verovio from '../vendor/verovio.js'
+
+export default class DummyClass {
+  constructor() {
+    let foo: any = new verovio.toolkit()
+  }
+}
